cd2cdef2a35d3bd831844e2a3fe9b4d6c51a53ea
MartinMwiti <martinmwiti007@gmail.com>
2021-07-27 14:21:40 +0300

    update

--- Rest_Api/Rest_Api/settings.py
+++ Rest_Api/Rest_Api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_i2bzd!#p4lw87(9qr@mh&r#eso)kez#5vt5&=4x(!%)7f3zj6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["closest-distance.herokuapp.com","localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
