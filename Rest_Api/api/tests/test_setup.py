939f1d960809da89cbf5eb3c0fd4c6b296b8203b
MartinMwiti <martinmwiti007@gmail.com>
2021-08-07 10:03:08 +0300

    added get request test

--- Rest_Api/api/tests/test_setup.py
+++ Rest_Api/api/tests/test_setup.py
@@ -4,11 +4,11 @@ from django.urls import reverse # takes a view name & gives a path to the route
 # Run all methods on starting the test
 class TestSetUp(APITestCase):
     
-    def setUp(self):
+    def setUp(self): # should Create test database
         self.getClosestDistance_url = reverse('getClosestDistance')
         self.calculateClosestDistance_url = reverse('calcClosestDistance')
         
         return super().setUp()
     
-    def tearDown(self):
+    def tearDown(self): # should destroy test database for alias 'default'...
         return super().tearDown()
--- Rest_Api/api/tests/test_views.py
+++ Rest_Api/api/tests/test_views.py
@@ -8,7 +8,7 @@ class TestViews(TestSetUp):
             "string_points": "John Doe"
         }
         # checks/assertions
-        res = self.client.post(self.calculateClosestDistance_url, data)      
+        res = self.client.post(self.calculateClosestDistance_url, data, format="json")      
         # pdb.set_trace()
         self.assertEqual(res.status_code, 400)
         
@@ -17,6 +17,13 @@ class TestViews(TestSetUp):
             "string_points": "(2,3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)"
         }
         # checks/assertions
-        res = self.client.post(self.calculateClosestDistance_url, data)      
+        res = self.client.post(self.calculateClosestDistance_url, data, format="json")      
+        # pdb.set_trace()
+        self.assertEqual(res.data['point'], data['string_points']) # whatever is coming back from DB is same as the keyed points
+        self.assertEqual(res.status_code, 200)
+        
+    def test_getClosestPoints_list(self):
+        # checks/assertions
+        res = self.client.get(self.getClosestDistance_url, format="json")      
         # pdb.set_trace()
         self.assertEqual(res.status_code, 200)
